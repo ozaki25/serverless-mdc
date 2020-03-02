fd6a2a09e5514d46054a6a6e206d9d1a1012827f
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-03 08:48:09 +0900

    put dynamo

--- handler.js
+++ handler.js
@@ -1,12 +1,38 @@
 'use strict';
 
-const getMessage = require('./getMessage');
+const AWS = require('aws-sdk');
 
-module.exports.hello = async (event, context, callback) => {
-  const message = getMessage(event);
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({ message }),
+const options = process.env.LOCAL
+  ? {
+      region: 'localhost',
+      endpoint: 'http://localhost:8081',
+    }
+  : {};
+
+const dynamo = new AWS.DynamoDB.DocumentClient(options);
+
+const tableName = 'sls-sample-dev-hello';
+
+module.exports.hello = async event => {
+  const params = {
+    TableName: tableName,
+    Item: { id: String(Date.now()), message: event },
   };
-  return callback(null, response);
+
+  try {
+    dynamo.put(params, (error, data) => {
+      if (error) throw new Error(error.stack);
+      console.log({ data });
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      };
+    });
+  } catch (e) {
+    console.log(e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: e.toString() }),
+    };
+  }
 };
