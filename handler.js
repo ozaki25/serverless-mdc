0cbad40b1eb4e48f8998311add10ee11596ddab4
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-05 00:24:58 +0900

    dynamodb settings

--- handler.js
+++ handler.js
@@ -3,36 +3,83 @@
 const AWS = require('aws-sdk');
 
 const options = process.env.LOCAL
-  ? {
-      region: 'localhost',
-      endpoint: 'http://localhost:8081',
-    }
+  ? { region: 'localhost', endpoint: 'http://localhost:8082' }
   : {};
 
 const dynamo = new AWS.DynamoDB.DocumentClient(options);
 
-const tableName = 'sls-sample-dev-hello';
+const tableName = process.env.tableName;
 
-module.exports.hello = async event => {
+module.exports.getAll = async () => {
+  const params = {
+    TableName: tableName,
+  };
+  console.log({ params });
+  try {
+    const result = await dynamo.scan(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+  } catch (error) {
+    console.log({ error });
+    return {
+      statusCode: error.statusCode,
+      body: error.message,
+    };
+  }
+};
+
+module.exports.get = async event => {
+  console.log({ event });
+  const { id } = event;
+  const params = {
+    TableName: tableName,
+    KeyConditionExpression: 'id = :id',
+    ExpressionAttributeValues: { ':id': id },
+  };
+  console.log({ params });
+  try {
+    const result = await dynamo.query(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+  } catch (error) {
+    console.log({ error });
+    return {
+      statusCode: error.statusCode,
+      body: error.message,
+    };
+  }
+};
+
+module.exports.put = async event => {
   const params = {
     TableName: tableName,
     Item: { id: String(Date.now()), message: event },
   };
 
+  console.log({ params });
+
   try {
-    dynamo.put(params, (error, data) => {
-      if (error) throw new Error(error.stack);
-      console.log({ data });
-      return {
-        statusCode: 200,
-        body: JSON.stringify(data),
-      };
-    });
-  } catch (e) {
-    console.log(e);
+    const result = await dynamo.put(params).promise();
     return {
-      statusCode: 500,
-      body: JSON.stringify({ message: e.toString() }),
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+  } catch (error) {
+    console.log({ error });
+    return {
+      statusCode: error.statusCode,
+      body: error.message,
     };
   }
+};
+
+module.exports.hello = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: event }),
+  };
 };
